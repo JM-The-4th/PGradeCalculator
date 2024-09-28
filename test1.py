22ca2c8d28b08f6b97416416284b66cda34c15de
JM-The-4th <enzo.monroy.iv@gmail.com>
2024-09-27 23:16:38 -0400

    Add comments and generalize data type to float

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 # Python Grade Calculator by Jesus Monroy IV
 
+#Declaring variables
 num_lab_probs = float(input("How many lab programming problems are completed?"))
 num_quiz_class = float(input("How many quizzes and in-class activities are completed?"))
 
@@ -17,15 +18,17 @@ grade_midterm_final_prep = float(input("What is the grade of the midterm and fin
 grade_lab_probs = 0
 grade_quiz_class = 0
 
+#Verifying lab and quiz numbers, then calculating their weighted grades
 if num_lab_probs >= 6:
-    grade_lab_probs = 20
+    grade_lab_probs = 20.0
 else:
     grade_lab_probs = num_lab_probs / 6 * 100 * 0.2
 if num_quiz_class >= 6:
-    grade_quiz_class = 15
+    grade_quiz_class = 15.0
 else:
     grade_quiz_class = num_quiz_class / 6 * 100 * 0.15
 
+#Calculating total weighted grade
 total_grade = round(grade_lab_probs + grade_quiz_class + grade_assignment1 + grade_assignment2 + grade_assignment3 + grade_assignment4 + grade_midterm1 + grade_midterm2 + grade_finalexam + grade_midterm_final_prep)
 
 print("Your grade is " + str(total_grade))
